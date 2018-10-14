50a796de0f2c1fbc83d09637c912180672b764af
syncrouteadmin <arshadk@syncroutesoftware.com>
2018-10-15 00:32:15 +0530

    tt.java changes

--- tt.java
+++ tt.java
@@ -1,7 +1,8 @@
-branch name :sep24
+branch name :sep24 to 15oct
+
+finally we get our old save file by using git stash pop
+
+git stash is use for save the our changes  before if we want to checkout the other branch first got for : git stash  ---> it will save your changes locallly 
 
 
-
-
-
-
+and again if you want come back in privous  or old brach and agian you want to get back your changes than go for : git stash pop
