b1c17104b6021e4c815f349acc004ad7e5d94cad
syncrouteadmin <arshadk@syncroutesoftware.com>
2018-10-15 12:20:03 +0530

    add all

--- tt.java
+++ tt.java
@@ -5,4 +5,4 @@ finally we get our old save file by using git stash pop
 git stash is use for save the our changes  before if we want to checkout the other branch first got for : git stash  ---> it will save your changes locallly 
 
 
-and again if you want come back in privous  or old brach and agian you want to get back your changes than go for : git stash pop
+and again if you want come back in privous  or old brach and agian you want to get back your changes than go for : git stash pop.
